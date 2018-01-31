1f90f538f23a520190247c9d2b281b9f318614a0
chloejk <chloejk@msn.com>
2018-01-31 21:34:41 +0100

    suite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131172056) do
+ActiveRecord::Schema.define(version: 20180131195927) do
+
+  create_table "gossips", force: :cascade do |t|
+    t.string "anonymous_author"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nom"
